1bcc2072ba93482da62355e0e1719cdb5a0f9a06
D4YonSoundcloud <mattd4y@gmail.com>
2020-03-21 15:20:38 -0400

    added button class 3:20pm 3-21-20

--- lavascript/script.js
+++ lavascript/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const mouse = {
     x: undefined,
@@ -25,11 +25,35 @@ class button {
     }
     update() {
         let directionX = 2.2;
-        if ((collision detection) && (button position)) {
-
-        } else if (not colliding with mouse and buttin is out of base position) {
-
+        if ((mouse.x < this.x + width && 
+            mouse.x > this.x && 
+            mouse.y < this.y + this.height && 
+            mouse.y > this.y
+            ) && (this.x > this.baseX - 50)) {
+                //animate button to the left
+                this.x -= directionX;
+        } else if (this.x < this.baseX)  {
+            this.x += directionX;
         }
+
+    }
+    draw() {
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.closePath()
     }
 }
 
+const buttons = []
+function createButtons() {
+    for (let i = 0; i < 5; i++) {
+        let topMargin = 100;
+        let buttonMargin = 5;
+        let x = 150;
+        let y = topMargin + ((50 + buttonMargin) * i);
+        let height = 50;
+        let width = 200;
+        buttons.push(new buttons(x,y,width, height))
+    }
+}
