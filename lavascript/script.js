e5ef5c67d966999f3e9769a5b103a2496024f5ad
d4yonsouncloude <mattd4y@gmail.com>
2020-03-22 14:51:03 -0600

    added container + more particles 2:50pm 3-22-20

--- lavascript/script.js
+++ lavascript/script.js
@@ -38,7 +38,6 @@ class Button {
     }
   }
   draw() {
-    ctx.fillStyle = "blue";
     ctx.beginPath();
     ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.closePath();
@@ -48,7 +47,7 @@ class Button {
 const buttons = [];
 function createButtons() {
   for (let i = 0; i < 5; i++) {
-    let topMargin = 160;
+    let topMargin = 158;
     let buttonMargin = 7;
     let x = 150;
     let y = topMargin + (50 + buttonMargin) * i;
@@ -67,7 +66,29 @@ function drawButtons() {
     buttons[i].draw();
   }
 }
+//other divs
+class Container {
+  constructor(x, y, width, height, baseX) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.baseX = x;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.closePath();
+  }
+}
 
+let container = new Container(360, 180, 550, 250);
+console.log(container);
+console.log("2");
+
+function drawContainer() {
+  container.draw();
+}
 //handle water particles
 class Particle {
   constructor(x, y, size, weight) {
@@ -77,42 +98,63 @@ class Particle {
     this.weight = weight;
     this.flowingRight = false;
   }
-  
+
   update() {
     //collision detection for particle/mouse
-    if( this.x > mouse.x - 50 &&
-        this.x < mouse.x + 50 &&
-        this.y > mouse.y - 5 &&
-        this.y < mouse.y + 5) {
-            this.x -= this.weight;
-            this.y += this.weight;
-            this.flowingRight = true;
-        }
+    if (
+      this.x > mouse.x - 50 &&
+      this.x < mouse.x + 50 &&
+      this.y > mouse.y - 5 &&
+      this.y < mouse.y + 5
+    ) {
+      this.x -= this.weight;
+      this.y += this.weight;
+      this.flowingRight = true;
+    }
+    //collision detection for container
+    if (
+      this.x < container.x + container.width &&
+      this.x > container.x &&
+      this.y < container.y + container.height &&
+      this.y > container.y
+    ) {
+      this.weight = 0;
+      if (!this.flowingRight) {
+        this.x -= 4;
+      } else {
+        this.x -= 4;
+      }
+    } else {
+      this.weight += 0.01;
+    }
     //collision detection for particle/button
-    for (let i = 0; i < buttons.length; i++){
-        if ( this.x < buttons[i].x + buttons[i].width &&
-            this.x > buttons[i].x && 
-            this.y < buttons[i].y + buttons[i].height &&
-            this.y > buttons[i].y) {
-                this.weight = 0;
-                if(!this.flowingRight){
-                    this.x -= 4;
-                } else {
-                    this.x += 4;
-                }
-            } else {
-                this.weight += 0.01;
-            }
+    for (let i = 0; i < buttons.length; i++) {
+      if (
+        this.x < buttons[i].x + buttons[i].width &&
+        this.x > buttons[i].x &&
+        this.y < buttons[i].y + buttons[i].height &&
+        this.y > buttons[i].y
+      ) {
+        this.weight = 0;
+        if (!this.flowingRight) {
+          this.x -= 4;
+        } else {
+          this.x += 4;
+        }
+      } else {
+        this.weight += 0.007;
+      }
     }
     if (this.y > canvas.height) {
       this.y = 0 - this.size;
-      this.x = Math.random() * 60 + 200;
+      this.x = Math.random()* 60 + this.x;
       this.weight = Math.random() * 0.2 + 0.1;
     }
     this.y += this.weight;
   }
+  
   draw() {
-    ctx.fillStyle = "rgba(128,197,222,1)";
+    ctx.fillStyle = "rgba(204, 0, 0,0.6)";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
@@ -123,15 +165,28 @@ const particleArray = [];
 const numberOfParticles = 80;
 function createParticles() {
   for (let i = 0; i < numberOfParticles; i++) {
-    const x = Math.random() * 60 + 300;
+    const x = Math.random() * 60 + 500;
     const y = Math.random() * canvas.height;
-    const size = Math.random() * 20 + 5;
+    const size = Math.random() * 20 + 10;
     const weight = Math.random() * 0.2 + 1;
     particleArray.push(new Particle(x, y, size, weight));
   }
 }
 
+const particleArray2 = [];
+const numberOfParticles2 = 80;
+function createMoreParticles2() {
+  for (let i = 0; i < numberOfParticles; i++) {
+    const x = Math.random() * 60 + 200;
+    const y = Math.random() * canvas.height;
+    const size = Math.random() * 20 + 10;
+    const weight = Math.random() * 0.2 + 1;
+    particleArray2.push(new Particle(x, y, size, weight));
+  }
+}
+
 createParticles();
+createMoreParticles2()
 
 // animate canvas
 function animate() {
@@ -140,7 +195,12 @@ function animate() {
     particleArray[i].update();
     particleArray[i].draw();
   }
+  for (let i = 0; i < particleArray2.length; i++) {
+    particleArray2[i].update();
+    particleArray2[i].draw();
+  }
   drawButtons();
+  drawContainer();
   requestAnimationFrame(animate);
 }
 
